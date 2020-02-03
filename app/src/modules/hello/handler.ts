5f01e3c5e586e7a1e8b5060a1272ba6168da48e2
Asif <asifmansoori@outlook.com>
2020-02-04 00:55:51 +0530

    added s3 events to lambda function and made required changes

--- app/src/modules/hello/handler.ts
+++ app/src/modules/hello/handler.ts
@@ -1,5 +1,33 @@
-export const hello = async (event: any, context: any) => {
-  console.log("ASIF: hello -> event", event);
+export const lambda1 = async (event: any, context: any) => {
+  console.log("ASIF: hello2 -> event", event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      // input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+export const lambda2 = async (event: any, context: any) => {
+  console.log("ASIF: hello2 -> event", event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      // input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+export const lambda3 = async (event: any, context: any) => {
+  console.log("ASIF: hello2 -> event", event);
   return {
     statusCode: 200,
     body: JSON.stringify({
